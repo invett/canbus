ff7ec32bf801a08849949d8ece7ce7d77a447474
Ruben <ruben.izquierdo@uah.es>
2023-06-13 17:55:06 +0200

    adding gitignore and little changes to the code (refactoring)

--- src/canbus_node.cpp
+++ src/canbus_node.cpp
@@ -4,20 +4,13 @@
 
 #include "BUSCAN.hpp"
 
-#include <sstream>
-
-/**
- * This tutorial demonstrates simple sending of messages over the ROS system.
- */
 int main(int argc, char **argv)
 {
   
-  ros::init(argc, argv, "talker");
+  ros::init(argc, argv, "canbus_driver");
   ros::NodeHandle n;
 
-  ros::Publisher chatter_pub = n.advertise<std_msgs::Float64>("speed", 1000);
-
-  ros::Rate loop_rate(10);
+  ros::Publisher speed_pub = n.advertise<std_msgs::Float64>("speed", 1);
 
   sem_t can_sem;
   sem_init(&can_sem, 0, 0);
@@ -28,56 +21,29 @@ int main(int argc, char **argv)
 
     if(init_buscan)
     {
-        std::cerr << "\033[1;32m"  << "BUSCAN inicializado " << "\033[0m" << std::endl;
+      ROS_INFO("BUSCAN inicializado");
+
+      double speed = 0;
+      C4speed_t *p;
+
+      while (ros::ok())
+      {
+
+        sem_wait(&can_sem);
+        std_msgs::Float64 msg;
+        msg.data = busCAN.getSpeedMPS_Unblocking(&p);
+
+        ROS_INFO("%lf", msg.data);
+        speed_pub.publish(msg);
+
+        ros::spinOnce();
+      }
+      return 1;
     }
     else
     {
-        std::cerr << "\033[1;31m" << "No se pudo inicializar el BUSCAN " << "\033[0m" <<std::endl;
-        ros::shutdown();
+      ROS_ERROR("No se pudo inicializar el BUSCAN ");
+      return 0;
     }
 
-  double speed = 0;
-  C4speed_t *p;
-
-  int count = 0;
-  while (ros::ok())
-  {
-
-    sem_wait(&can_sem);
-    std_msgs::Float64 msg;
-    msg.data = busCAN.getSpeedMPS_Unblocking(&p);
-
-    ROS_INFO("%lf", msg.data);
-    chatter_pub.publish(msg);
-
-    ros::spinOnce();
-
-    //loop_rate.sleep();
-    ++count;
-  }
-
-
-  return 0;
 }
-
-
-
-
-
-// //-- BUSCAN --
-//     m_busCAN_initialized = false;
-//     //--------------------------------------------------------------------------------------------------------------
-//     //---------- BUSCAN Initialization -----------------------------------------------------------------------------
-//     {
-//         m_busCAN = new BUSCAN(HLC_sem);
-//         m_busCAN_initialized = m_busCAN->init_BUSCAN(BAUD_500K); //NO COFIGURABLE
-
-//         if(m_busCAN_initialized)
-//         {
-//             std::cerr << "\033[1;32m"  << "BUSCAN inicializado " << "\033[0m" << std::endl;
-//         }
-//         else
-//         {
-//             std::cerr << "\033[1;31m" << "No se pudo inicializar el BUSCAN " << "\033[0m" <<std::endl;
-//         }
-//     }
